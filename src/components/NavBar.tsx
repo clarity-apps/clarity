aa901b8eafa9158584d160c94c4db613b6c1081a
Walter Li <li.walter1@gmail.com>
2023-05-03 17:59:08 -0400

    styling: navbar anchor text, awaiting images and modal

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,26 @@
 function NavBar() {
   return (
-    <nav className="nav">
-      <div className="navbar-left">
-        <a href="/">Clarity</a>
-        <a href="/practice">Practice</a>
-        <a href="/about">About</a>
-      </div>
-      <div className="navbar-right">
-        <a href="/potd">POTD</a>
-        <a href="/profile">Profile</a>
+    <nav className="nav-root">
+      <div className="navbar">
+        <div className="navbar-left">
+          <a className="nav-item" href="/">
+            Clarity
+          </a>
+          <a className="nav-item" href="/practice">
+            Practice
+          </a>
+          <a className="nav-item" href="/about">
+            About
+          </a>
+        </div>
+        <div className="navbar-right">
+          <a className="nav-item" href="/potd">
+            POTD
+          </a>
+          <a className="nav-item" href="/profile">
+            Profile
+          </a>
+        </div>
       </div>
     </nav>
   );
